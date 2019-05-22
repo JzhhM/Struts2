af0b2de4ded14098d5305600de1ac29f27f5135b
zhj <zhj@DESKTOP-5HE117A>
2019-05-22 16:12:13 +0800

    Struts2_demo1学习

--- src/com/mhys/demo2/ActionDemo2.java
+++ src/com/mhys/demo2/ActionDemo2.java
@@ -4,6 +4,12 @@ import com.opensymphony.xwork2.Action;
 
 /**
  * Action的编写方式二：实现Action的接口
+ * 	实现接口的方式：提供了5个常量（5个逻辑视图名称）
+ * 		*SUCCESS :成功
+ * 		*ERROR ：失败
+ * 		*LOGIN ：登陆出错页面跳转
+ * 		*INPUT : 表单验证的时候出错
+ * 		*NONE  ：不跳转
  * @author zhj
  *
  */
